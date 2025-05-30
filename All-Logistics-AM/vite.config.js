065c11d4ba299d9e9026163fc03c655ce9b7912a
Jsra11 <131608620+Jsra11@users.noreply.github.com>
2025-05-30 09:30:21 -0400

    otro

--- All-Logistics-AM/vite.config.js
+++ All-Logistics-AM/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './All-Logistics-AM',
   plugins: [react()
     , tailwindcss()
   ],
