e4870e542144e13936b8f335648906087d221cee
Jsra11 <131608620+Jsra11@users.noreply.github.com>
2025-05-30 09:23:50 -0400

    arreglandoerrores

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
